f858859cdf37654b7f744f0d43cb7cf700b1a493
ShivanshKumar760 <shivanshkumar760@gmail.com>
2026-06-29 04:22:53 +0530

    testing ci/cd pipline

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app=Flask(__name__)
+const_internal_db=[]
 
 @app.route('/',methods=["GET"])
 def home():
-    return jsonify({"msg":"Hello,World Version 2"})
+    return jsonify({"msg":"Hello,World Version 3 deployed from another os"})
 
+@app.route('/create',methods=["POST"])
+def create():
+    data=request.get_json()
+    new_item={
+        "id":len(const_internal_db)+1,
+        "name":data.get("name")
+    }
+    const_internal_db.append(new_item);
+    return jsonify(new_item),201
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)  
