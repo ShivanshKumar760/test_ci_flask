37b83002e7828b7e5014720a7e23c63e6551729c
ShivanshKumar760 <shivanshkumar760@gmail.com>
2026-07-08 07:37:12 +0530

    db.py typo post->port

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ load_dotenv()
 def get_connection():
     return psycopg2.connect(
         host = os.getenv("POSTGRES_HOST","localhost"),
-        post = os.getenv("POSTGRES_PORT","5432"),
+        port = os.getenv("POSTGRES_PORT","5432"),
         dbname = os.getenv("POSTGRES_DB","blog_db"),
         user = os.getenv("POSTGRES_USER","bloguser"),
         password = os.getenv("POSTGRES_PASSWORD","blogpass")
